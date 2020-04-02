ab1553d2db0564944b745a423f212e1b18530102
Gensor <Gensor@users.noreply.github.com>
2020-04-02 22:59:57 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ziackaknizka/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.ziackaknizka;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+//        Vytvorte aplikáciu, ktorá bude zobrazovať zoznam študentov.
+//        Po kliknutí na študenta sa v novom okne zobrazia jeho známky oddelené čiarkou.
+//        Pri výbere položky editovať alebo pridať sa zobrazí zoznam známok a bude možné pridať študentovi novú známku alebo upraviť existujúce známky.
+
+
+
+
+
+    }
+}
